1ee720e52428fda72467606c02ff3763b602dc31
kush-pixel <patel.h.kush@gmail.com>
2025-11-01 14:40:41 -0500

    added folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "RAG Project",
+    version = '0.0.0',
+    author = 'Kush Patel',
+    packages= find_packages(),
+    isntall_requires = []
+)
